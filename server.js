182f65bb82b8ec9d7308e0c9f0550ac8186b0bc2
Gail Matsushima <gail.matsushima@gmail.com>
2016-02-13 16:10:15 -1000

    listening to server

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 var express = require('express');
 var mongoose = require('mongoose');
-var path = require('path');
 var morgan = require('morgan');
 var methodOverride = require('method-override');
 var bodyParser = require('body-parser');
 
-mongoose.connect('mongodb://localhost/pixelpainter_mongoose_demo');
+mongoose.connect('mongodb://localhost/pixelpainter');
 var pixelSchema = mongoose.Schema({owner: String});
-//intsantiating Painting (model), drawings (database) which mongo creates
+//intsantiating Painting (model), paintings (database) which mongo creates
 var Painting = mongoose.model('Painting', pixelSchema);
 
 var app = express();
@@ -15,4 +14,14 @@ var app = express();
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(morgan('dev'));
 app.use(methodOverride('_method'));
-app.use(express.static(path.resolve(__dirname, 'public')));
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error'));
+//db.once is an event listener, and now app.listen can be a call back
+  db.once('open', function (){
+    console.log('db connected');
+    app.listen(3000, function () {
+      console.log('listening on 3000');
+    });
+  });
+
