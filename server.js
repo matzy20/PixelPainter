7bd45a4dd953cac02c565422ca8f284270a3a71a
Gail Matsushima <gail.matsushima@gmail.com>
2016-02-13 16:45:24 -1000

    got PixelPainters index get route working

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 var express = require('express');
+var path = require('path');
 var mongoose = require('mongoose');
 var morgan = require('morgan');
 var methodOverride = require('method-override');
 var bodyParser = require('body-parser');
-
+//mongo is running on in terminal
 mongoose.connect('mongodb://localhost/pixelpainter');
 var pixelSchema = mongoose.Schema({owner: String});
 //intsantiating Painting (model), paintings (database) which mongo creates
@@ -14,14 +15,29 @@ var app = express();
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(morgan('dev'));
 app.use(methodOverride('_method'));
+app.use(express.static(path.resolve(__dirname, 'Public')));
+
+app.set('views', 'views');
+app.set('view engine', 'jade');
+
+app.get('/paintings', function (req, res){
+  Painting.find({},function (err, paintings){
+    if(err){
+      res.send(err + 'is not valid');
+    }
+    console.log('getting paintings');
+    res.render('index');
+  });
+});
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error'));
 //db.once is an event listener, and now app.listen can be a call back
-  db.once('open', function (){
-    console.log('db connected');
-    app.listen(3000, function () {
-      console.log('listening on 3000');
-    });
+db.once('open', function (){
+  console.log('db connected');
+  //what express is running on
+  app.listen(3000, function () {
+    console.log('listening on 3000');
   });
+});
 
